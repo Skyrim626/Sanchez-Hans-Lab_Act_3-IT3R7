fbd55f3cd376402407357e7a7fd3d9f1f5e23de6
Hans Zin <sanchezhanszin@gmail.com>
2023-12-02 11:38:59 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:form_validation/signUp_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sanchez, Hans Zin Activity',
+      debugShowCheckedModeBanner: false,
+      home: SignUp(),
+    );
+  }
+}
